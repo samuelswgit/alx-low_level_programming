312973e1815833a9b2363987f85f3e8976c81d61
samuelswgit <samuel@northstar-alliance.org>
2023-03-21 07:45:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 # include <stdio.h>
+/*puts */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
